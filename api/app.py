001492b9e12efe0d3f9e7d1670453ef2c3ff1648
Muhammad Yoosoofsah <muhammad.yoosoofsah17@imperial.ac.uk>
2023-10-16 16:22:34 +0100

    first commit

---
+++ api/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    input_name = request.form.get("name")
+    input_age = request.form.get("age")
+    input_team = request.form.get("team")
+    return render_template("hello.html", name=input_name, age=input_age, team=input_team)
+
