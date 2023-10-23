0d300f6a2443e0d8b994d7fc65e828de799d6201
Tiril Mageli <tiril.mageli23@imperial.ac.uk>
2023-10-23 15:31:11 +0100

    message

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 
@@ -8,13 +10,15 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+
 @app.route("/submit", methods=["POST"])
 
+
 def submit():
-    input_name = request.form.get("name")
-    input_age = request.form.get("age")
-    input_city = request.form.get("city")
-    return render_template("hello.html",
-        name = input_name,
-      	age = input_age,
-	      city = input_city)
+  input_name = request.form.get("name")
+  input_age = request.form.get("age")
+  input_city = request.form.get("city")
+  return render_template("hello.html",
+                         name = input_name,
+                         age = input_age,
+                         city = input_city)
