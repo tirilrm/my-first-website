18a0f3b934375c1a2c1f07dab3030022e52fce0e
Tiril Mageli <tiril.mageli23@imperial.ac.uk>
2023-10-23 16:00:59 +0100

    final fixes

--- api/app.py
+++ api/app.py
@@ -5,14 +5,11 @@ app = Flask(__name__)
 
 
 @app.route("/")
-
-
 def hello_world():
     return render_template("index.html")
 
+
 @app.route("/submit", methods=["POST"])
-
-
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
