63749f3e3a9dfcebc4b0dedde0c75effc27984dc
Tiril Mageli <tiril.mageli23@imperial.ac.uk>
2023-10-23 17:40:35 +0100

    test again

--- api/app_test.py
+++ api/app_test.py
@@ -1,15 +1,15 @@
 from app import app
 import unittest
 
+
 class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-
     def test_knows_about_dinosaurs(self):
         response = self.app.get('/query?q=dinosaurs')
-        self.assertEqual(response.data.decode('utf-8'), "Dinosaurs ruled the Earth 200 million years ago")
-
+        self.assertEqual(response.data.decode('utf-8'),
+                         "Dinosaurs ruled the Earth 200 million years ago")
 
     def test_does_not_know_about_asteroids(self):
         response = self.app.get('/query?q=asteroids')
