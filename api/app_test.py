33789c23a5983b6d3f047082acfecd4462396d9a
Tiril Mageli <tiril.mageli23@imperial.ac.uk>
2023-10-23 17:38:15 +0100

    amendment

--- api/app_test.py
+++ api/app_test.py
@@ -1,11 +1,20 @@
-from app import process_query
+from app import app
+import unittest
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
 
 
-def test_knows_about_dinosaurs():
-    assert process_query("dinosaurs") == (
-        "Dinosaurs ruled the Earth 200 million years ago"
-    )
+    def test_knows_about_dinosaurs(self):
+        response = self.app.get('/query?q=dinosaurs')
+        self.assertEqual(response.data.decode('utf-8'), "Dinosaurs ruled the Earth 200 million years ago")
 
 
-def test_does_not_know_about_asteroids():
-    assert process_query("asteroids") == "Unknown"
+    def test_does_not_know_about_asteroids(self):
+        response = self.app.get('/query?q=asteroids')
+        self.assertEqual(response.data.decode('utf-8'), "Unknown")
+
+
+if __name__ == '__main__':
+    unittest.main()
