d9a3dc51ae2d719b9abd63c7b6b407ab82d0cd42
Tiril Mageli <tiril.mageli23@imperial.ac.uk>
2023-10-23 15:35:30 +0100

    fixed final flake8 errors

--- api/app.py
+++ api/app.py
@@ -10,15 +10,14 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
-
 @app.route("/submit", methods=["POST"])
 
 
 def submit():
-  input_name = request.form.get("name")
-  input_age = request.form.get("age")
-  input_city = request.form.get("city")
-  return render_template("hello.html",
-                         name = input_name,
-                         age = input_age,
-                         city = input_city)
+    input_name=request.form.get("name")
+    input_age=request.form.get("age")
+    input_city=request.form.get("city")
+    return render_template("hello.html",
+                           name=input_name,
+                           age=input_age,
+                           city=input_city)
